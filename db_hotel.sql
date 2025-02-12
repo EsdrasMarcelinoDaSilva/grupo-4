3ff850a3f65fd0bbae17c6e07fcba5cf728474ef
Tamara Farias <email do git>
2025-02-12 16:23:11 -0300

    populei a tabela hospede

--- db_hotel.sql
+++ db_hotel.sql
@@ -6,3 +6,11 @@ CREATE TABLE hospede (
      email VARCHAR(100) UNIQUE NOT NULL,
   	 
 );
+
+insert into hospede (nome, email) values 
+('Dario Silva', 'dariosilva@gmail.com'),
+('Julia Vegas', 'juliavegas@gmail.com'),
+('Rebeca Santos', 'rebecasantos@gmail.com'),
+('Maria Oliveira', 'mariaoliveira@gmail.com'),
+('Carla Martins', 'carlamartins@gmail.com'),
+('Roberta Campos', 'robertacampos@gmail.com')
