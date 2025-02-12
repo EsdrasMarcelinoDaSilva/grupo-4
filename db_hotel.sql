7a1a66c308d711b61f591b21e4b3730993520c89
Tamara Farias <email do git>
2025-02-12 16:32:50 -0300

    criei a tabela reserva e projetei dados dela

--- db_hotel.sql
+++ db_hotel.sql
@@ -18,3 +18,21 @@ insert into hospede (nome, email) values
 select * from hospede
 
 select nome, id from hospede where nome like '%Roberto%';
+
+CREATE TABLE reserva (
+   id SERIAL PRIMARY KEY,
+   hospede_id INT NOT NULL,
+   data_reserva TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+   FOREIGN KEY (hospede_id) REFERENCES hospede(id) ON DELETE CASCADE
+);
+
+INSERT INTO reserva (hospede_id, data_reserva) VALUES
+(3, '2025-02-10 12:30:00'),
+(4, '2025-02-10 13:30:00'),
+(5, '2025-02-11 14:30:00'),
+(6, '2025-02-12 15:30:00'),
+(7, '2025-02-13 16:30:00'),
+(8, '2025-02-14 17:30:00')
+(9, '2025-02-30' '20:30:00')
+
+select * from reserva
